1b1d3d4029b6f49602fa7686d9340b898c71cd8b
AminulSagor <aminulislamsagor3@gmail.com>
2025-04-29 19:00:18 +0600

    Fix incoming call popup delay by immediately showing dialog for caller

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,37 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller = window?.rootViewController as! FlutterViewController
+    let screenProtectionChannel = FlutterMethodChannel(name: "screen_protection",
+                                                       binaryMessenger: controller.binaryMessenger)
+
+    screenProtectionChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      if call.method == "enableProtection" {
+        // Enable iOS screen protection
+        controller.view.window?.layer.superlayer?.addSublayer(self.getScreenProtectionLayer())
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func getScreenProtectionLayer() -> CALayer {
+    let layer = CALayer()
+    layer.frame = UIScreen.main.bounds
+    layer.backgroundColor = UIColor.black.cgColor
+    layer.opacity = 0.01 // Light transparent layer to prevent screen recording
+    return layer
   }
 }
